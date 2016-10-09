1307cc8bcea4929b991541e4d162d97efeb9537d
Tomas Simacek <simacektomas@volny.cz>
2016-10-10 01:00:30 +0200

    Heuristic

--- MI-PAA/Homework/Knapsack_problem/knapsack.cpp
+++ MI-PAA/Homework/Knapsack_problem/knapsack.cpp
@@ -5,10 +5,29 @@
 
 using namespace std;
 
+struct KnapsackItem {
+	public:
+		KnapsackItem();
+		KnapsackItem( int weight, int price )
+		:m_weight(weight), m_price(price), m_ratio(double(price)/double(weight)){};
+		void print(){ cout << m_weight << ":" << m_price << ":" << m_ratio;}; 
+
+		int m_weight;
+		int m_price;
+		double m_ratio; 
+};
+
 
 class KnapsackProblem {
 	public:
 		KnapsackProblem( string instance );
+		~KnapsackProblem(){
+			for ( int i = 0; i < m_count; ++i ){
+				delete m_items[i];
+			}
+			delete [] m_items;
+
+		};
 		int solveBruteForce();
 		int solveHeuristic();
 		void showParameters(){
@@ -20,7 +39,7 @@ class KnapsackProblem {
 		int m_id;
 		int m_count;
 		int m_knapsack;
-		int * m_instance;
+		KnapsackItem ** m_items;
 
 };
 
@@ -35,7 +54,43 @@ KnapsackProblem::KnapsackProblem( string instance ){
 	m_count = std::stoi( token );
 	iss >> token;
 	m_knapsack = std::stoi( token );	
-	
+
+	m_items = new KnapsackItem*[m_count];
+	string price, weight;
+
+	for ( int i = 0; i < m_count; ++i ){
+		iss >> weight;
+		iss >> price;
+		m_items[i] = new KnapsackItem( std::stoi(weight), std::stoi(price) );
+		m_items[i]->print();
+		cout << " ";
+
+	}
+	cout << endl;
+}
+
+int KnapsackProblem::solveBruteForce(){
+
+}
+
+int heuristicCompare( const void * a, const void * b ){
+	const KnapsackItem * item1 = *(const KnapsackItem **) a;
+	const KnapsackItem * item2 = *(const KnapsackItem **) b;
+
+	return ( item1->m_ratio - item2->m_ratio );
+}
+
+int KnapsackProblem::solveHeuristic(){
+
+	KnapsackItem ** heuristic = new KnapsackItem*[m_count];
+	for ( int i = 0; i < m_count; ++i ) heuristic[i] = m_items[i];
+
+	//Sort the array by the heuristic ratio
+	qsort( heuristic, m_count, sizeof(KnapsackItem*), heuristicCompare );
+	for ( int i = 0; i < m_count; ++i ){ heuristic[i]->print(); cout << "|"; };
+	cout << endl;
+
+	delete [] heuristic;
 
 }
 
@@ -50,7 +105,7 @@ int main ( int args, char ** argv ){
 		while ( getline( dataFile, instance ) ){
 			
 			KnapsackProblem knapsackInstance( instance );	
-			knapsackInstance.showParameters();
+			knapsackInstance.solveHeuristic();
 		}
 
 		dataFile.close();
